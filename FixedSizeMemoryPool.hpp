f8b606c2295d8c884c867fdf26056df77c06da60
zepeh <zepeh92@gmail.com>
2021-02-06 21:09:00 +0900

    인덴트 수정

--- FixedSizeMemoryPool.hpp
+++ FixedSizeMemoryPool.hpp
@@ -5,101 +5,101 @@
 
 class FixedSizeMemoryPool
 {
-  public:
+public:
 
-	FixedSizeMemoryPool(
-		size_t segmentSize, 
-		size_t defaultGrowth = 64) noexcept :
-		_segmentSize{ segmentSize < sizeof(void*) ? sizeof(void*) : segmentSize },
-		_defaultGrowthSize{ defaultGrowth == 0 ? 1 : defaultGrowth }
-	{
-	}
+  FixedSizeMemoryPool(
+    size_t segmentSize, 
+    size_t defaultGrowth = 64) noexcept :
+    _segmentSize{ segmentSize < sizeof(void*) ? sizeof(void*) : segmentSize },
+    _defaultGrowthSize{ defaultGrowth == 0 ? 1 : defaultGrowth }
+  {
+  }
 
-	FixedSizeMemoryPool(const FixedSizeMemoryPool& other) = delete;
+  FixedSizeMemoryPool(const FixedSizeMemoryPool& other) = delete;
 
-	FixedSizeMemoryPool(FixedSizeMemoryPool&& other) = delete;
+  FixedSizeMemoryPool(FixedSizeMemoryPool&& other) = delete;
 
-    ~FixedSizeMemoryPool()
+  ~FixedSizeMemoryPool()
+  {
+    while (_lastAllocPage != nullptr)
     {
-		while (_lastAllocPage != nullptr)
-		{
-			void* nextPage = NextOf(_lastAllocPage);
+      void* nextPage = NextOf(_lastAllocPage);
 
-			free(_lastAllocPage);
-			
-			_lastAllocPage = nextPage;
-		}
+      free(_lastAllocPage);
+      
+      _lastAllocPage = nextPage;
+    }
+  }
+
+  inline void* Allocate() noexcept
+  {
+    if (IsEmpty())
+    {
+      if (!Grow(_defaultGrowthSize))
+      {
+        return nullptr;
+      }
     }
 
-    inline void* Allocate() noexcept
+    void* ret = _frontSegment;
+
+    _frontSegment = NextOf(_frontSegment);
+
+    return ret;
+  }
+
+  inline void Deallocate(void* ptr) noexcept
+  {
+    NextOf(ptr) = _frontSegment;
+
+    _frontSegment = ptr;
+  }
+
+  inline size_t GetSegmentSize() const noexcept
+  {
+    return _segmentSize;
+  }
+
+  inline bool IsEmpty() const noexcept
+  {
+    return _frontSegment == nullptr;
+  }
+
+  bool Grow(size_t segmentCount) noexcept
+  {
+    const size_t newPageSize = sizeof(void*) + (_segmentSize * segmentCount);
+    
+    char* ptr = static_cast<char*>(malloc(newPageSize));
+    if (ptr == nullptr)
     {
-		if (IsEmpty())
-		{
-			if (!Grow(_defaultGrowthSize))
-			{
-				return nullptr;
-			}
-		}
-
-		void* ret = _frontSegment;
-
-		_frontSegment = NextOf(_frontSegment);
-
-		return ret;
+      return false;
     }
 
-	inline void Deallocate(void* ptr) noexcept
-	{
-		NextOf(ptr) = _frontSegment;
+    NextOf(ptr) = _lastAllocPage;
+    _lastAllocPage = ptr;
 
-		_frontSegment = ptr;
-	}
+    char* endOfNewPage = ptr + newPageSize;
+    ptr += sizeof(void*); 
 
-	inline size_t GetSegmentSize() const noexcept
-	{
-		return _segmentSize;
-	}
-
-    inline bool IsEmpty() const noexcept
+    while (ptr != endOfNewPage)
     {
-		return _frontSegment == nullptr;
+      NextOf(ptr) = _frontSegment;
+      _frontSegment = ptr;
+      ptr += _segmentSize;
     }
 
-	bool Grow(size_t segmentCount)
-	{
-		const size_t newPageSize = sizeof(void*) + (_segmentSize * segmentCount);
-		
-		char* ptr = static_cast<char*>(malloc(newPageSize));
-		if (ptr == nullptr)
-		{
-			return false;
-		}
+    return true;
+  }
 
-		NextOf(ptr) = _lastAllocPage;
-		_lastAllocPage = ptr;
+private:
 
-		char* endOfNewPage = ptr + newPageSize;
-		ptr += sizeof(void*); 
+  static inline void*& NextOf(void* pageOrSegment) noexcept
+  {
+    return *static_cast<void**>(pageOrSegment);
+  }
 
-		while (ptr != endOfNewPage)
-		{
-			NextOf(ptr) = _frontSegment;
-			_frontSegment = ptr;
-			ptr += _segmentSize;
-		}
-
-		return true;
-	}
-
-  private:
-
-    static inline void*& NextOf(void* pageOrSegment) noexcept
-    {
-		return *static_cast<void**>(pageOrSegment);
-    }
-
-    void* _lastAllocPage = nullptr;
-    void* _frontSegment = nullptr;
-	const size_t _segmentSize;
-    const size_t _defaultGrowthSize;
+  void* _lastAllocPage = nullptr;
+  void* _frontSegment = nullptr;
+  const size_t _segmentSize;
+  const size_t _defaultGrowthSize;
 };
